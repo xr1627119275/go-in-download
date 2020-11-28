9582256986e3b76209e45c9b28cbd3e959c9d56b
xrdev <1627119275@qq.com>
2020-11-28 23:43:34 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+#docker run --name download  -itd -p 1280:1280 -v /var/file:/bin/file  --restart=always --privileged=true registry.cn-hangzhou.aliyuncs.com/xrdev/go-in-download:$1
+# Preparing the build environment
+FROM golang:1.15-alpine AS builder
+
+WORKDIR $GOPATH/src/goindownload
+COPY . $GOPATH/src/goindownload
+RUN GOPROXY="https://goproxy.io" GO111MODULE=on go build .
+
+# Preparing the final image
+FROM alpine:3.12.1
+EXPOSE 1280
+
+COPY --from=builder /go/src/goindownload/goIndownload /bin/goIndownload
+COPY --from=builder /go/src/goindownload/html /bin/html
+
+WORKDIR /bin
+ENTRYPOINT [ "/bin/goIndownload" ]
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Status int
+
+const (
+	Downloading      Status = 0
+	DownloadComplete Status = 1
+	DownloadFail     Status = 2
+	Installing       Status = 3
+	InstallComplete  Status = 4
+	InstallFail      Status = 5
+)
+
+type Manage struct {
+	ID         string `json:"id"`
+	FileName   string `json:"file_name"`
+	Status     Status `json:"status"`
+	Msg        string `json:"msg"`
+	Downloader *Downloader
+}
+
+type Downloader struct {
+	io.Reader       `json:"_"`
+	Total           int64                `json:"total"`
+	Current         int64                `json:"current"`
+	DownloadSuccess func(manage *Manage) `json:"_"`
+}
+
+var DownloadManage []*Manage
+
+func (d *Downloader) Read(p []byte) (n int, err error) {
+	n, err = d.Reader.Read(p)
+	d.Current += int64(n)
+	//log.Printf("\r正在下载，下载进度：%.2f%%", float64(d.Current*10000/d.Total)/100)
+	//if d.Current == d.Total {
+	//	log.Printf("\r下载完成，下载进度：%.2f%%", float64(d.Current*10000/d.Total)/100)
+	//}
+	return
+}
+func DownloadFile(url, filePath string) (*Manage, error) {
+	manage := &Manage{
+		ID:         strconv.Itoa(int(time.Now().UnixNano())),
+		FileName:   "",
+		Status:     Downloading,
+		Msg:        "",
+		Downloader: nil,
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		manage.Status = DownloadFail
+		manage.Msg = "请求文件下载出错"
+		log.Printf("请求文件下载出错: %v", err.Error())
+		return nil, err
+	}
+
+	downloader := &Downloader{
+		Reader: resp.Body,
+		Total:  resp.ContentLength,
+	}
+	manage.Downloader = downloader
+
+	DownloadManage = append(DownloadManage, manage)
+
+	var filename string
+	Disposition := resp.Header.Get("Content-Disposition")
+	r := regexp.MustCompile(`filename=(.+)`)
+	if r.MatchString(Disposition) {
+		filename = r.FindStringSubmatch(Disposition)[1]
+	} else {
+		filename = manage.ID
+
+		if path.Base(url) != "" {
+			filename = path.Base(url)
+			filename = strings.ReplaceAll(filename, "?", "_")
+			filename = strings.ReplaceAll(filename, "&", "_")
+		}
+	}
+	manage.FileName = filename
+	fmt.Println("  Disposition :", Disposition)
+	fmt.Println("  fileName :", manage.FileName)
+	go func(manage *Manage) {
+		os.Remove(path.Join(filePath, filename))
+
+		file, _ := os.Create(path.Join(filePath, filename))
+
+		defer func() {
+			_ = resp.Body.Close()
+			_ = file.Close()
+			if downloader.DownloadSuccess != nil {
+				downloader.DownloadSuccess(manage)
+			}
+
+			go func() {
+				time.Sleep(time.Minute * 5)
+				err := os.Remove(path.Join(filePath, filename))
+				if err != nil {
+					log.Printf("%v 删除文件出错: %v", filename, err.Error())
+				}
+			}()
+		}()
+
+		if _, err := io.Copy(file, downloader); err != nil {
+			log.Printf("下载文件出错: %v", err.Error())
+		}
+
+		manage.Status = DownloadComplete
+
+	}(manage)
+
+	return manage, nil
+}
+
+func download(c *gin.Context) {
+	url, _ := c.GetQuery("url")
+	manage, _ := DownloadFile(url, "file")
+	//if err != nil {
+	//	c.JSON(200, gin.H{ "ID": "" })
+	//}
+	//c.JSON(200, gin.H{ "ID": manage.ID })
+	c.JSON(200, manage)
+}
+func progress(c *gin.Context) {
+	id, _ := c.GetQuery("id")
+
+	for i := 0; i < len(DownloadManage); i++ {
+		if DownloadManage[i].ID == id {
+			c.JSON(200, DownloadManage[i])
+			return
+		}
+	}
+
+	c.JSON(200, nil)
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+
+		router := gin.Default()
+		router.GET("/api/download", download)
+		router.GET("/api/progress", progress)
+		router.StaticFS("/static", http.Dir("./html"))
+		router.StaticFS("/api/file", http.Dir("./file"))
+
+		router.Run(":1280")
+	}()
+
+	wg.Wait()
+}
